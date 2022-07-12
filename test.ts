07aa60914d5790984548638925a59602834555f1
Okoye Victor <okoyeebuka64@gmail.com>
2022-07-12 15:41:04 +0100

    More Test Cases

--- test.ts
+++ test.ts
@@ -53,7 +53,7 @@ function greet(person: String) {
       console.log(x!);
   }
   live(4);
-  // we can also add ? to show that it is not a most that there is gonna be an argument.
+  // we can also add ? to show that it is not a most that there is gonna be an argument/it is optional
   
   
   const printAll = (strs: string | string[] | null) => {
@@ -113,4 +113,82 @@ function greet(person: String) {
           return Math.PI * shape.radius ** 2;
       }
   };
-  
+
+
+//   function Types
+
+function greeter(fn: (a: string )=> void) {
+    fn("Hello World")
+}
+//   fxn with one parameter that is a string that doesnt return any value 
+// alternatively we can set it as a type alias
+type greetfunction = (a: string) => void;
+function greeter1(fn: greetfunction) {
+console.log();
+};
+ type describeFxn = {
+     description : string;
+     (someArg: number) : boolean ;
+ };
+
+ function doStuff(fn : describeFxn) {
+     console.log(fn.description + "returned" + fn(6));;
+     return(fn.description + "returned" + fn(6));
+ }
+
+
+
+//  generic
+const exe = (arr: any[])=>{
+    console.log(arr[1]);
+    return arr[1]
+}
+
+exe(["hey", "test"]);
+
+// can be modified to return the type of the array passed in instead of 'any'
+const example = <Type>(arr: Type[]) : Type | undefined =>{
+    console.log(arr[3]);
+    return arr[1]
+}
+
+example([1,2,3,"hello", true, null]);
+
+const s = example(["hi", 'read', 'try', 'fast']);
+
+// constraints
+
+const longest = <Type extends { length : number}> (a: Type, b: Type)=> {
+    if (a.length > b.length){
+        console.log(a);
+        return a;
+    }
+    else {
+        console.log(b);
+        return b;
+    }
+}
+
+longest([9,2,1,2], [0,2,1]);
+longest("hello", "hey");
+
+
+// declare function f(x? : number) : void;
+
+
+// never== shows that the return property doesnt have any value
+function fail(msg: string) : never {
+    throw new Error(msg)
+}
+
+// rest parameter
+const multiply = (n: number, ...m: number[])=>{
+    return m.map((x)=> n * x);
+}
+console.log(multiply(5,4,3,2,1)); 
+
+
+// spread argument
+const args = [3,4] as const
+const angle = Math.atan2(...args);
+
